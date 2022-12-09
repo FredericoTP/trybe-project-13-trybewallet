558ec6f2c88b2c0cae8c5d80d37a94c35fa01b26
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-12-08 22:05:12 -0300

    Requisito 6

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </div>
     );
   }
 }
 
-export default Table;
+export default connect()(Table);
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 import { requestCurrencies } from '../redux/actions';
 
 class Wallet extends React.Component {
@@ -16,7 +17,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <WalletForm />
-        <p>TrybeWallet</p>
+        <Table />
       </div>
     );
   }
