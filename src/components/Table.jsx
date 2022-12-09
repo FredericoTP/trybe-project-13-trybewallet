487a224cc8c6e70908a970c1fe9b023e0a4d074b
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2022-12-08 22:48:57 -0300

    Requisito 7

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,24 +1,64 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            expenses.map((item) => {
+              const {
+                description, tag, method, value, id, currency, exchangeRates,
+              } = item;
+              const exchangeValue = +exchangeRates[currency].ask;
+              return (
+                <tr key={ id }>
+                  <td>{ description }</td>
+                  <td>{ tag }</td>
+                  <td>{ method }</td>
+                  <td>{ (+value).toFixed(2) }</td>
+                  <td>{ exchangeRates[currency].name }</td>
+                  <td>{ exchangeValue.toFixed(2) }</td>
+                  <td>{ (+value * exchangeValue).toFixed(2) }</td>
+                  <td>Real</td>
+                  <td>b</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
     );
   }
 }
 
-export default connect()(Table);
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string,
+      currency: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
