1866006537e07ae5daaae0823d4adcb52fc123ab
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-01-05 13:37:55 -0300

    Setup gh-pages

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <Provider store={ store }>
-      <BrowserRouter>
+      <BrowserRouter basename={ process.env.PUBLIC_URL }>
         <App />
       </BrowserRouter>
     </Provider>,
